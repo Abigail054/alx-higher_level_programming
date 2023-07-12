0d1378497ff24d2e91f21cc0646ac4c235af2301
Abigail054 <dekuabiflimsy@gmail.com>
2023-07-11 22:31:22 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
+if tens_digit in range(10):
+    if ones_digit in range(tens_digit + 1, 10):
         print(f"{tens_digit}{ones_digit:02d}", end=', ')
 print()
