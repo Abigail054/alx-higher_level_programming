53c3e5ad0db33e85c2aa39dfe77b5a07310ef07e
Abigail054 <dekuabiflimsy@gmail.com>
2023-07-11 20:14:05 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-        if number > 0
-print(f"{number} is positive)
+        if number > 5
+print(f"{number} and is greater)
         elif number == 0
-print (f{number} is zero)
-        else
-print(f{number} is negative)
+print (f{number} and is zero)
+        else if number < 6
+print(f{number} and is less)
