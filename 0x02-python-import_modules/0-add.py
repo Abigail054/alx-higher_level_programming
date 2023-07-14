c9a976b6d30d4a82cd79eed3aed29c8dba43eec4
Abigail054 <dekuabiflimsy@gmail.com>
2023-07-13 18:05:43 -0700

    python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-import importlib.util
-
-spec = importlib.util.spec_from_file_location("add_0", "add_0.py")
-add_0 = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(add_0)
-
-result = add_0.add(a, b)
-
-print(f"{a} + {b} = {result}")
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
